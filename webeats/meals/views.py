10984cd13ff5fae0201b004bbcc6e3a48e8e1864
maximilian <maximilian@kindshofer.net>
2016-09-18 14:04:05 +0200

    make a view and urls

--- webeats/meals/views.py
+++ webeats/meals/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.forms import formset_factory
+from django.contrib.auth.decorators import login_required
+from . import forms
 
-# Create your views here.
+@login_required
+def add_dish(request):
+
+    user = request.user
+    IngredientFormset = formset_factory(forms.IngredientForm)
+
+    if request.method == 'POST':
+        dish_form = forms.DishForm(request.POST)
+        ingredient_formset = forms.IngredientFormset(request.POST)
+        
+        if dish_form.is_valid() and ingredient_formset.is_valid():
+            dish = Dish.objects.create(name=dish_form.cleaned_data.get('name'),
+                                       picture=dish_form.cleaned_data.get('picture'))
+        
+        for ingredient in ingredient_formset:
+            Ingredient.objects.create(name=ingredient.cleaned_data.get('name'),
+                                      amount=ingredient.cleaned_data.get('amount'),
+                                      unit=ingredient.cleaned_date.get('unit'),
+                                      dish=dish)
+    else:
+        dish_form = forms.DishForm()
+        ingredient_formset = IngredientFormset()
+
+    context = {
+        'dish_form': dish_form,
+        'ingredient_formset': ingredient_formset,
+    }
+
+    return render(request, 'meals/add_dish.html', context)            
