7c0b9c1908a669b284f6892f5598dc36d7830223
Maximilian Kindshofer <maximilian@kindshofer.net>
2016-10-22 23:03:00 +0200

    getting tired

--- webeats/profiles/wunderlist_utils.py
+++ webeats/profiles/wunderlist_utils.py
@@ -1,5 +1,5 @@
 
-def get_authorization_url(client_id, redirect_url, state)
+def get_authorization_url(client_id, redirect_url, state):
     return authorize_url = "https://www.wunderlist.com/oauth/authorize?client_id={}&redirect_uri={}&state={}".format(client_id, redirect_url, state)
 
 
